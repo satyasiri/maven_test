832cd3c75bfa74a9402275e4c96c6b0042e628fc
satyasiri <satyasirisha3@gmail.com>
2019-01-23 15:14:56 +0530

    adding sample mven projectt

---
+++ test_artifact/src/main/java/test_artifact/test_class.java
@@ -0,0 +1,9 @@
+package test_artifact;
+
+public class test_class {
+	public String abc()
+	{
+		return "welcome";
+	}
+
+}
---
+++ test_artifact/src/test/java/test_artifact/test_class_junit.java
@@ -0,0 +1,14 @@
+package test_artifact;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test_class_junit {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
